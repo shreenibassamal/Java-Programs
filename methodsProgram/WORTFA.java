76e28379433cf8c95d95ffe5b54788448220e8ad
Shreenibas Samal <shreenibassamal@gmail.com>
2024-12-10 11:13:02 +0530

    Method without return-type and with-out formal arguments

---
+++ methodsProgram/WORTFA.java
@@ -0,0 +1,19 @@
+package methodsProgram;
+
+public class WORTFA {
+    //This method it WITH-OUT RETUEN & FORMAL ARGUMENTS.
+
+    public static void fun1() {
+        int a = 100;
+        int b = 50;
+        System.out.println("this method fun1() is with-out RT & FA  and addition of the given number is: "+(a+b));
+    }
+    public static void main(String[] args) {
+        System.out.println("starting of main method");
+        fun1();
+        System.out.println("ending of main method");
+
+
+    }
+
+}
